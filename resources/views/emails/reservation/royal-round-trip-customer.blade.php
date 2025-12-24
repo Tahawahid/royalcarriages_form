063ad37338c8fa2f4293b18adf5a7567cc2a8ebd
Tahawahid <muhammadtahawahid1@gmail.com>
2025-12-24 22:06:54 +0500

    fixed the number

--- resources/views/emails/reservation/royal-round-trip-customer.blade.php
+++ resources/views/emails/reservation/royal-round-trip-customer.blade.php
@@ -9,7 +9,7 @@
 
     <p>Thank you for your interest in Royal Carriages limousines & Charter Worldwide! We have received your reservation request and our team will get back to you once we enter your reservation in the system. We're excited to help make your transportation experience exceptional!</p>
 
-    <p>If you need immediate assistance or have questions about your reservation you can reach our office line 24/7 Monday-Sunday 713-974-5466.</p>
+    <p>If you need immediate assistance or have questions about your reservation you can reach our office line 24/7 Monday-Sunday 713-787-5466.</p>
 
     <p>Phone: 713-787-5466<br>
     Email: info@royalcarriages.com</p>
